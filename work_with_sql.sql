221a86505f837bf84fcc5742691c372c247bd48b
Netology_n <an.zaickova.rr@yandex.ru>
2023-12-11 14:29:34 +0300

    Initial commit

--- work_with_sql.sql
+++ work_with_sql.sql
@@ -1,48 +1,43 @@
-create table if not exists albumsmusicians(
-albumsmusicians_id SERIAL primary key,
-album_id INTEGER not null references album(album_id),
-musician_id INTEGER not null references musician(musician_id)
+CREATE TABLE IF NOT EXISTS albumsmusicians(
+albumsmusicians_id SERIAL PRIMARY KEY,
+album_id INTEGER NOT NULL REFERENCES album(album_id),
+musician_id INTEGER NOT NULL REFERENCES musician(musician_id)
 );
 
-create table if not exists musiciansgenres(
-musiciansgenres_id SERIAl primary key,
-musician_id INTEGER not null references musician(musician_id),
-genre INTEGER not null references genre(genre_id)
+CREATE TABLE IF NOT EXISTS musiciansgenres(
+musiciansgenres_id SERIAl PRIMARY KEY,
+musician_id INTEGER NOT NULL REFERENCES musician(musician_id),
+genre_id INTEGER NOT NULL REFERENCES genre(genre_id)
 );
 
-create table if not exists musician(
-musician_id SERIAL primary key,
-name VARCHAR(100) not null,
-genre_id INTEGER not null references genre(genre_id),
-album_id INTEGER not null references album(album_id)
+CREATE TABLE IF NOT EXISTS musician(
+musician_id SERIAL PRIMARY KEY,
+name VARCHAR(40) NOT NULL UNIQUE
 );
 
-create table if not exists genre(
-genre_id SERIAL primary key, 
-name VARCHAR (100) not null, 
-musician_id INTEGER references musician(musician_id)
+CREATE TABLE IF NOT EXISTS genre(
+genre_id SERIAL PRIMARY KEY, 
+name VARCHAR (40) NOT NULL UNIQUE 
 );
 
-create table if not exists album(
-album_id SERIAL primary key,
-name VARCHAR(100) not null,
-year_of_release date not null,
-musician_id INTEGER not null references musician(musician_id)
+CREATE TABLE IF NOT EXISTS album(
+album_id SERIAL PRIMARY KEY,
+name VARCHAR(40) NOT NULL,
+year_of_release date NOT NULL CHECK (year_of_release>2000-01-01)
 );
 
-create table if not exists song(
-song_id SERIAL primary key,
-name VARCHAR(100) not null,
-duration INTEGER not null,
-album_id INTEGER not null references album(album_id),
-collection_id INTEGER not null references collection(collection_id)
+CREATE TABLE IF NOT EXISTS song(
+song_id SERIAL PRIMARY KEY,
+name VARCHAR(40) NOT NULL,
+duration INTEGER NOT NULL CHECK (duration >60),
+album_id INTEGER NOT NULL REFERENCES album(album_id),
+collection_id INTEGER NOT NULL REFERENCES collection(collection_id)
 );
 
-create table if not exists collection(
-collection_id SERIAL primary key,
-name VARCHAR(100) not null,
-year_of_release date not null,
-song_id INTEGER not null references song(song_id)
+CREATE TABLE IF NOT EXISTS collection(
+collection_id SERIAL PRIMARY KEY,
+name VARCHAR(40) NOT NULL,
+year_of_release date NOT NULL CHECK (year_of_release>2000-01-01)
 );
 
 
